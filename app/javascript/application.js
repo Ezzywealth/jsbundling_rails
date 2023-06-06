b9b1fb7652fbb7c7c08fb7982480c74985e66b0c
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-06 20:01:32 +0100

    Setup react router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-	return (
-		<>
-			<HelloWorld />
-		</>
-	);
-}
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HelloWorld />,
+	},
+]);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<RouterProvider router={router} />
 	</Provider>,
 	document.getElementById('root')
 );
